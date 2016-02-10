ce05aca537114ea5e8d7d4c2480604d01814b382
Will Spurgin <will.spurgin@gmail.com>
2016-02-10 15:48:15 -0600

    Update for Project with Joe.

--- scraper/ironman.py
+++ scraper/ironman.py
@@ -7,32 +7,45 @@ import urllib
 import requests
 import re
 
-class Ironman(object):
-    """The Tony Stark of web crawler classes. Yolo."""
-    def __init__(self, **kwargs):
-        super(Ironman, self).__init__()
-        if kwargs['target_url']:
-            self.target_url = kwargs['target_url']
-        else:
-            self.target_url = None
-        if kwargs['pattern']:
-            self.pattern = kwargs['pattern']
-        else:
-            self.pattern = None
+from robotparser import RobotFileParser
+from urlparse import urlparse
 
-    def read_data(self):
-        if not self.target_url:
+class Ironman(object):
+    """
+    The Tony Stark of web crawler classes. Yolo.
+    @param starting_url
+    @param treat_as_host = false
+    @kwargs
+        pattern = regex pattern used to detect new URLS
+        bind_to_starting_host = boolean
+
+    The `treat_as_host` option is useful if the starting URL is not actually a
+    host such as http://lyle.smu.edu/~wspurgin
+    """
+    def __init__(self, starting_url, treat_as_host=False, **kwargs):
+        super(Ironman, self).__init__()
+        self.starting_url = starting_url
+        self.treat_as_host = treat_as_host
+        if self.treat_as_host:
+            self.host = self.starting_url
+        else:
+            self.host = urlparse(self.starting_url).netloc
+        self.pattern = re.compile('<a\s?.*href="(.+\.\w+)"\s*>')
+        if 'pattern' in kwargs:
+            self.pattern = re.compile(kwargs['pattern'])
+
+        # Get Robots.txt and parse it for information.
+
+    def crawl(self):
+        if not self.starting_url:
             raise Exception("No url set for instance of Ironman")
-        if not self.pattern:
-            raise Exception("No pattern set for instance of Ironman")
-        node = requests.get(self.target_url)
+        node = requests.get(self.starting_url)
         matches = re.findall(self.pattern, node.content)
         return matches
 
 if __name__=="__main__":
     # sample testing
-    url = "http://www.fairlabor.org/affiliates/participating-companies"
-    pattern = '<a href="/affiliate/(.*)">' #regex pattern to get parent names
-    yolo = Ironman(target_url=url, pattern=pattern)
-    parents = yolo.read_data()
-    print set(parents)
+    starting_url = "http://lyle.smu.edu/~fmoore"
+    yolo = Ironman(starting_url, treat_as_host=True)
+    nodes = yolo.crawl()
+    print set(nodes)
